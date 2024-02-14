34f3a77b14d403b9bd777016642ba31314a3695e
Rachel Esteve <r.esteve@uwtsd.ac.uk>
2024-02-14 00:19:08 +0000

    added api keys and linked javascript

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+var appId="566894"
+var accessKey="yWH9hjEhFXjQhvtQPA8HDnyVzi_ocqp0JvHQDdzE9jA"
+var secretKey="tLxOXBxVmBk3PeUzYuiAhr1MWH-HTTy3Crg4fIDaax8"
+const testUrl="https://api.unsplash.com/search/photos?page=1&query=office"
+fetch(testUrl).then(response=> console.log(response))
