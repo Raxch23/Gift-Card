cf3b7e3a7efa3254ad1a8eabec6e8fa8c89f4399
Rachel Esteve <r.esteve@uwtsd.ac.uk>
2024-02-16 20:50:55 +0000

    created color mode

--- assets/js/image.js
+++ assets/js/image.js
@@ -4,25 +4,41 @@ imageTag.style.width="600px"
 imageTag.src=userImage 
 var cardBox=document.createElement("div")
 cardBox.setAttribute("id", "card-box")
-cardBox.style.border="2px solid red"
 cardBox.append(imageTag)
 document.querySelector("#single-picture").append(cardBox)
 var yourCards=[]
 document.getElementById("message-form").addEventListener("submit", function(event){
     event.preventDefault()
+    console.log(event.target[3].checked)
+    var colorTheme=""
+    if(event.target[3].checked){
+        colorTheme="light"
+    }
+    else{
+        colorTheme="dark"
+    }
     var cardObject={
         imgsrc:userImage,
         to:document.getElementById("subject").value,
         message:document.getElementById("message").value,
         from:document.getElementById("from").value,
-
+        colorTheme
     }
+    console.log(cardObject)
     yourCards.push(cardObject)
     localStorage.setItem("yourcards", JSON.stringify(yourCards))
     var messageCard=document.createElement("div")
-    messageCard.setAttribute("class", "card message-card")
+    messageCard.setAttribute("data-color", cardObject.colorTheme)
+    messageCard.setAttribute("class", "message-card")
+    if(messageCard.getAttribute("data-color")==="light"){
+        messageCard.style.color="white"
+    }
     var subjectLine=document.createElement("h3")
-    subjectLine.textContent=cardObject.to
-    messageCard.append(subjectLine)
+    subjectLine.textContent="to: "+cardObject.to+","
+    var messageLine=document.createElement("h4")
+    messageLine.textContent=cardObject.message
+    var closingLine= document.createElement("h4")
+    closingLine.textContent="from: "+cardObject.from;
+    messageCard.append(subjectLine, messageLine, closingLine)
     document.querySelector("#card-box").append(imageTag, messageCard)
 })
