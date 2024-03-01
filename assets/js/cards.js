a6b34bb23c8d7dc6886226b4437e0f79c384b6fd
Rachel Esteve <r.esteve@uwtsd.ac.uk>
2024-03-01 17:13:22 +0000

    created save card page, cleanned code, added save button, fix positioning and added margin to pictures

--- assets/js/cards.js
+++ assets/js/cards.js
@@ -1,28 +1,55 @@
-const cardArray=JSON.parse(localStorage.getItem("yourcards"))
-let saveCard=document.getElementById("save-card")
-cardArray.forEach(card => {
-    console.log(card)
-    var cardDIV=document.createElement("div")
-    cardDIV.setAttribute( "class", "card col-4")
-    var cardImg=document.createElement("img")
-    cardDIV.style.width="500px"
-    cardImg.setAttribute("class", "card-img-top")
-    cardImg.src=card.imgsrc; 
-    var messageCard=document.createElement("div")
-    messageCard.setAttribute("data-color", card.colorTheme)
-    messageCard.setAttribute("class", "message-card")
-    if(messageCard.getAttribute("data-color")==="light"){
-        messageCard.style.color="white"
-    }
-    var subjectLine=document.createElement("h3")
-    subjectLine.textContent="To: "+card.to+","
-    var messageLine=document.createElement("h4")
-    messageLine.textContent=card.message
-    var closingLine= document.createElement("h4")
-    closingLine.textContent="From: "+card.from;
-    messageCard.append(subjectLine, messageLine, closingLine)
-    cardDIV.append(messageCard)
-    //document.querySelector("#card-box").append(imageTag, messageCard)
-    cardDIV.append(cardImg)
-    saveCard.append(cardDIV)
-});
+const cardArray = JSON.parse(localStorage.getItem("yourcards"))
+let saveCard = document.getElementById("save-card")
+
+
+function buildCards(){
+    saveCard.innerHTML=""
+    cardArray.forEach(card => {
+        
+        var cardDIV = document.createElement("div")
+        cardDIV.style.width = "400px";
+        cardDIV.style.position="relative"
+        cardDIV.style.marginBottom="5px"
+        var cardImg = document.createElement("img")
+        cardImg.setAttribute("class", "card-img-top")
+        cardImg.src = card.imgsrc;
+        cardImg.style.width="100%"
+        var messageCard = document.createElement("div")
+        messageCard.setAttribute("data-color", card.colorTheme)
+        messageCard.setAttribute("class", "message-card")
+        messageCard.classList.add(card.position)
+        messageCard.classList.add(card.colorTheme)
+        var subjectLine = document.createElement("h3")
+        subjectLine.textContent = "To: " + card.to + ","
+    
+        var messageLine=document.createElement("h4")
+        messageLine.textContent=card.message
+    
+        var closingLine= document.createElement("h4")
+        closingLine.textContent="From: "+card.from;
+    
+        messageCard.append(subjectLine, messageLine, closingLine)
+        cardDIV.append(cardImg, messageCard)
+        saveCard.append(cardDIV)
+    
+    });
+}
+buildCards()
+// cardArray.forEach(card => {
+
+
+//     messageCard.setAttribute("data-color", card.colorTheme)
+//     messageCard.setAttribute("class", "message-card")
+//     if(messageCard.getAttribute("data-color")==="light"){
+//         messageCard.style.color="white"
+//     }
+//     
+
+//     var closingLine= document.createElement("h4")
+//     closingLine.textContent="From: "+card.from;
+//     messageCard.append(subjectLine, messageLine, closingLine)
+//     cardDIV.append(messageCard)
+//     //document.querySelector("#card-box").append(imageTag, messageCard)
+//     cardDIV.append(cardImg)
+//     saveCard.append(cardDIV)
+// });
--- assets/js/image.js
+++ assets/js/image.js
@@ -1,59 +1,64 @@
-var userImage=localStorage.getItem("user-image")
-var imageTag=document.createElement("img")
-//imageTag.style.width="600px"
-imageTag.src=userImage 
-var cardBox=document.createElement("div")
+var userImage = localStorage.getItem("user-image")
+
+var imageTag = document.createElement("img")
+imageTag.src = userImage;
+
+var cardBox = document.createElement("div")
 cardBox.setAttribute("id", "card-box")
 cardBox.append(imageTag)
+
 document.querySelector("#single-picture").append(cardBox)
-var yourCards=JSON.parse(localStorage.getItem("yourcards"))||[]
-console.log(yourCards)
-document.getElementById("message-form").addEventListener("submit", function(event){
+
+var yourCards = JSON.parse(localStorage.getItem("yourcards")) || []
+
+document.getElementById("message-form").addEventListener("submit", function (event) {
     event.preventDefault()
-    var position=document.querySelector(".form-select").value
-    console.log(position)
-    console.log(event.target[3].checked)
-    var colorTheme=""
-    if(event.target[3].checked){
-        colorTheme="light"
+    var messageCard = document.createElement("div")
+    cardBox.innerHTML=""
+    var position = document.querySelector(".form-select").value
+    console.log(event.target[4])
+    var colorTheme = ""
+    if (event.target[4].checked) {
+        colorTheme = "light"
     }
-    else{
-        colorTheme="dark"
+    else {
+        colorTheme = "dark"
     }
-    var cardObject={
-        imgsrc:userImage,
-        to:document.getElementById("subject").value,
-        message:document.getElementById("message").value,
-        from:document.getElementById("from").value,
+    var cardObject = {
+        imgsrc: userImage,
+        to: document.getElementById("subject").value,
+        message: document.getElementById("message").value,
+        from: document.getElementById("from").value,
         colorTheme,
         position
     }
-    console.log(cardObject)
+
     localStorage.setItem("currentcard", JSON.stringify(cardObject))
-    var messageCard=document.createElement("div")
     messageCard.setAttribute("data-color", cardObject.colorTheme)
     messageCard.setAttribute("class", cardObject.position)
-    if(messageCard.getAttribute("data-color")==="light"){
-        messageCard.style.color="white"
+
+    if (messageCard.getAttribute("data-color") === "light") {
+        messageCard.style.color = "white"
     }
-    var subjectLine=document.createElement("h3")
-    subjectLine.textContent="To: "+cardObject.to+","
-    var messageLine=document.createElement("h4")
-    messageLine.textContent=cardObject.message
-    var closingLine= document.createElement("h4")
-    closingLine.textContent="From: "+cardObject.from;
+    var subjectLine = document.createElement("h3")
+    subjectLine.textContent = "To: " + cardObject.to + ","
+    var messageLine = document.createElement("h4")
+    messageLine.textContent = cardObject.message
+    var closingLine = document.createElement("h4")
+    closingLine.textContent = "From: " + cardObject.from;
     messageCard.append(subjectLine, messageLine, closingLine)
     //imageTag.append(messageCard)
     document.querySelector("#card-box").append(imageTag, messageCard)
     //document.getElementById("form-column").style.display="none"
     //document.getElementById("button-column").style.display="block"
 })
-document.getElementById("reset").addEventListener("click", function(){
-    document.location.href="index.html"
+document.getElementById("reset").addEventListener("click", function () {
+    document.location.href = "index.html"
 })
-document.getElementById("save").addEventListener("click", function(){
-    var cardObject=JSON.parse(localStorage.getItem("currentcard"))
+document.getElementById("save").addEventListener("click", function () {
+  
+    var cardObject = JSON.parse(localStorage.getItem("currentcard"))
     yourCards.push(cardObject)
     localStorage.setItem("yourcards", JSON.stringify(yourCards))
-    document.location.href="yourcards.html"
+    document.location.href = "yourcards.html"
 })
